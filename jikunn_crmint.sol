bea626578059134c154f8834505b4e7ac11cc38a
Joshua James Arcelao <100921013+Jikunnn@users.noreply.github.com>
2024-11-22 18:22:58 +0800

    Add files via upload

---
+++ jikunn_crmint.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/access/Ownable.sol";
+
+contract JikunnToken is ERC20, Ownable {
+    string private constant _name = "JikunnToken";
+    string private constant _symbol = "JKT";
+    uint8 private constant _decimals = 18;
+    uint256 private _initialSupply = 100 * 10**uint256(_decimals);
+
+    constructor() ERC20(_name, _symbol) {
+        _mint(msg.sender, _initialSupply);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function burnFrom(address account, uint256 amount) external {
+        uint256 currentAllowance = allowance(account, msg.sender);
+        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
+        _approve(account, msg.sender, currentAllowance - amount);
+        _burn(account, amount);
+    }
+}
